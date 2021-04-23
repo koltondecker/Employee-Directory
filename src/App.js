a0d7517e8c1703378943ad97fa88f13f26f07813
Kolton Decker <deckerman25@gmail.com>
2021-04-23 18:14:26 -0500

    Added search bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Jumbotron from "./components/Jumbotron";
+import SearchBar from './components/searchBar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Jumbotron />
+        <SearchBar />
       </header>
     </div>
   );
---
+++ src/components/searchBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function searchBar() {
+    return (
+        <div className="input-group mb-3">
+            <input type="text" className="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1"/>
+        </div>
+    )
+}
+
+export default searchBar
